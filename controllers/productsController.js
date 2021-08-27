9deacf37477c08cc02e8080babeebb50e5e6ca05
EricM76 <53233372+EricM76@users.noreply.github.com>
2021-08-27 19:03:10 -0300

    agrega & valida productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
-const categories = require('../data/categories.json')
+const categories = require('../data/categories.json');
+
+const {validationResult} = require('express-validator');
 
 module.exports = {
     search : (req,res) => {
@@ -19,11 +21,42 @@ module.exports = {
         })
     },
     store : (req,res) => {
-        res.send(req.body)
+        let errors = validationResult(req);
+        if (req.fileValidationError) {
+            let image = {
+                param : 'image',
+                msg: req.fileValidationError,
+            }
+            errors.errors.push(image)
+        }
+        if(errors.isEmpty()){
+            const {name, description, category} = req.body;
+            let photos = [];
+            req.files.forEach(image => {
+                photos.push(image.filename)
+            });
+            let product = {
+                id : products.length != 0 ? products[products.length - 1].id + 1 : 1,
+                name : name.trim(),
+                description : description.trim(),
+                category,
+                photos
+            }
+            products.push(product);
+            fs.writeFileSync(path.join(__dirname,'..','data','products.json'),JSON.stringify(products,null,2),'utf-8');
+            res.redirect('/')
+        }else{
+          return res.render('productAdd',{
+              categories,
+              errors : errors.mapped(),
+              old : req.body
+          })
+        }
     },
     edit : (req,res) => {
         return res.render('productEdit',{
-            categories
+            categories,
+            product :products.find(producto => producto.id === +req.params.id)
         })
     },
     update : (req,res) => {
--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {search, add, store, edit, update, destroy, detail} = require('../controllers/productsController');
 
+const productImageStore = require('../middlewares/productImageStore');
+const productsAddValidator = require('../validations/productsAddValidator');
+
 router.get('/search',search);
 router.get('/detail/:id',detail);
 router.get('/add',add);
-router.post('/add',store);
+router.post('/add',productImageStore.array('image'), productsAddValidator, store);
 router.get('/edit/:id',edit);
 router.put('/update/:id',update);
  router.delete('/destroy/:id',destroy);
